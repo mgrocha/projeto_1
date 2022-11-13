b83c3d0aba30304f1b85b7ba882fccefda523818
Maura Rocha <maura.rocha@confitec.com.br>
2022-11-13 12:20:12 -0300

    projeto usando método

--- projeto.rb
+++ projeto.rb
@@ -1,31 +1,45 @@
-subtotal = 0
-loop do
-    puts "Selecione a opção desejada:: "
+$subtotal = 0.00
+$valorproduto = [50.00,200.00,100.00]
+
+def voltamenuprincipal
+    puts "Digite 0 para voltar ao menu inicial:"
+    opvolta = gets.chomp.to_i
+    menuprincipal
+end
+
+def precototal (produto,qtd)
+    $subtotal += $valorproduto[produto-1] * qtd
+    puts "Subtotal R$ #{$subtotal}"
+    voltamenuprincipal
+end
+
+def menuprodutos
+    puts "Selecione o produto desejado:"
+    puts "[ 1 ] Produto A: R$ 50,00"
+    puts "[ 2 ] Produto B: R$ 200,00"
+    puts "[ 3 ] Produto C: R$ 100,00"
+    produto = gets.chomp.to_i
+    puts "Digite a quantidade desejada:"
+    qtd = gets.chomp.to_i
+    precototal(produto,qtd)
+end
+
+def opcao(op)
+    if op == 2
+        puts "Até breve!!!"
+    elsif op == 1
+        menuprodutos
+    else
+        menuprincipal
+    end
+end
+
+def menuprincipal
+    puts "Selecione a opção desejada:"
     puts "[ 1 ] Comprar"
     puts "[ 2 ] Sair"
     operacao = gets.chomp.to_i
-
-        case operacao
-            when 1
-                puts "Selecione a opção desejada:: "
-                puts "[ 1 ] Produto A: R$ 50,00"
-                puts "[ 2 ] Produto B: R$ 200,00"
-                puts "[ 3 ] Produto C: R$ 100,00"
-                produto = gets.chomp.to_i
-                puts "Digite a quantidade desejada:"
-                qtd = gets.chomp.to_i
-                if produto == 1
-                    subtotal = 50.00*qtd+subtotal
-                end
-                puts "Subtotal R$ #{subtotal}"
-                puts "Digite 0 para voltar ao menu inicial:"
-                opvolta = gets.chomp.to_i
-            when 2
-                puts "Até breve!!!"
-                break
-        end
-    if opvolta != 0
-        puts "Digite 0 para voltar ao menu inicial:"
-        opvolta = gets.chomp.to_i
-    end
+    opcao(operacao)
 end
+
+responda = menuprincipal
